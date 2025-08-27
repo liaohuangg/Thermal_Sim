c3370a9a527ce226eae23aed587c6572cfeefbcf
liaohuangg <liaohuangg@gmail.com>
2025-08-26 23:59:36 -0500

    add simulator

---
+++ Simulator/runenv.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export RUN_SH="/root/Experiment/Thermal_Sim/Simulator/config/run.sh"
+export EXAMPLE_CONFIG="/root/Experiment/Thermal_Sim/Simulator/config/example.config"
+export GCC_PTRACE="/root/Experiment/Thermal_Sim/Simulator/config/gcc.ptrace"
+export EXAMPLE_MATERIALS="/root/Experiment/Thermal_Sim/Simulator/config/example.materials"
+export EV6_FLP="/root/Experiment/Thermal_Sim/Simulator/config/ev6.flp"
+export EV6_TYPE="/root/Experiment/Thermal_Sim/Simulator/config/ev6.type"
+
+# python optimize.py > /root/Experiment/Thermal_Sim/output/log.log 2>&1
